ca6d415e8647eefbd2abec3c58f8e8420aa231b0
David <pr0v0ziert@gmx.de>
2022-10-13 07:29:35 +0700

    Update client.ts

--- packages/examples/browser-lsp/src/client.ts
+++ packages/examples/browser-lsp/src/client.ts
@@ -17,6 +17,7 @@ import 'monaco-editor/esm/vs/editor/standalone/browser/referenceSearch/standalon
 import 'monaco-editor/esm/vs/editor/standalone/browser/toggleHighContrast/toggleHighContrast.js';
 
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api.js';
+import normalizeUrl from 'normalize-url';
 
 import { buildWorkerDefinition } from 'monaco-editor-workers';
 
@@ -24,6 +25,8 @@ import { MonacoLanguageClient, CloseAction, ErrorAction, MonacoServices, Message
 import { BrowserMessageReader, BrowserMessageWriter } from 'vscode-languageserver-protocol/browser.js';
 import { StandaloneServices } from 'vscode/services';
 import getMessageServiceOverride from 'vscode/service-override/messages';
+import { toSocket, WebSocketMessageReader, WebSocketMessageWriter } from 'vscode-ws-jsonrpc';
+
 
 StandaloneServices.initialize({
     ...getMessageServiceOverride(document.body)
@@ -33,20 +36,18 @@ buildWorkerDefinition('dist', new URL('', window.location.href).href, false);
 
 // register Monaco languages
 monaco.languages.register({
-    id: 'plaintext',
-    extensions: ['.txt'],
-    aliases: ['PLAINTEXT', 'plaintext'],
-    mimetypes: ['text/plain']
+    id: 'python',
+    extensions: ['.py'],
+    aliases: ['Python', 'python'],
+    //mimetypes: ['text/plain']
 });
 
 // create Monaco editor
-const editorText = `
-#ff0000 (red)
-#00ff00 (green)
-#0000ff (blue)
+const editorText = `def myfunc(x):
+    return x
 `;
 monaco.editor.create(document.getElementById('container')!, {
-    model: monaco.editor.createModel(editorText, 'plaintext', monaco.Uri.parse('inmemory://model.txt')),
+    model: monaco.editor.createModel(editorText, 'python', monaco.Uri.parse('inmemory://model.py')),
     glyphMargin: true,
     lightbulb: {
         enabled: true
@@ -58,7 +59,7 @@ function createLanguageClient (transports: MessageTransports): MonacoLanguageCli
         name: 'Sample Language Client',
         clientOptions: {
             // use a language id as a document selector
-            documentSelector: [{ language: 'plaintext' }],
+            documentSelector: [{ language: 'python' }],
             // disable the default error handler
             errorHandler: {
                 error: () => ({ action: ErrorAction.Continue }),
@@ -73,14 +74,57 @@ function createLanguageClient (transports: MessageTransports): MonacoLanguageCli
         }
     });
 }
+function createLanguageClient2 (transports: MessageTransports): MonacoLanguageClient {
+    return new MonacoLanguageClient({
+        name: 'Sample Language Client',
+        clientOptions: {
+            // use a language id as a document selector
+            documentSelector: ['python'],
+            // disable the default error handler
+            errorHandler: {
+                error: () => ({ action: ErrorAction.Continue }),
+                closed: () => ({ action: CloseAction.DoNotRestart })
+            }
+        },
+        // create a language client connection from the JSON RPC connection on demand
+        connectionProvider: {
+            get: () => {
+                return Promise.resolve(transports);
+            }
+        }
+    });
+}
 
 // install Monaco language client services
 MonacoServices.install();
-
+/* 
 const worker = new Worker(new URL('./src/serverWorker.ts', window.location.href).href, { type: 'module' });
 const reader = new BrowserMessageReader(worker);
 const writer = new BrowserMessageWriter(worker);
 const languageClient = createLanguageClient({ reader, writer });
 languageClient.start();
 
-reader.onClose(() => languageClient.stop());
+reader.onClose(() => languageClient.stop()); */
+
+
+
+function createUrl (hostname: string, port: number, path: string): string {
+    const protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+    return normalizeUrl(`${protocol}://${hostname}:${port}${path}`);
+}
+
+
+const url = createUrl('localhost', 3001, '/python');
+const webSocket = new WebSocket(url);
+
+webSocket.onopen = () => {
+    const socket = toSocket(webSocket);
+    const reader = new WebSocketMessageReader(socket);
+    const writer = new WebSocketMessageWriter(socket);
+    const languageClient = createLanguageClient({
+        reader,
+        writer
+    });
+    languageClient.start();
+    reader.onClose(() => languageClient.stop());
+};
